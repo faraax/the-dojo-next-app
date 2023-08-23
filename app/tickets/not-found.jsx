16c4ce7512e52a9eb65a7903ee499cebc76f51a1
Faraax <suny.faraz@gmail.com>
2023-08-23 20:48:39 +0500

    Lesson # 9

---
+++ app/tickets/not-found.jsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Notfound() {
+    return (
+        <main className='text-center'>
+            <h2 className='text-3xl'>We Hit a Brick Wall</h2>
+            <p>We could not find the tickets you were looking for</p>
+            <p>Go back to all <Link href={'/tickets'}>Tickets</Link></p>
+        </main>
+    )
+}
